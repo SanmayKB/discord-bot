e2a5fa43e1c1d6b83409e3be6dff9509bf92312e
Link <alexrexgamers@gmail.com>
2024-12-07 13:53:46 +0530

    added dot env safety for token

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config;
 const {Client, IntentsBitField} = require('discord.js');
 
 const client = new Client({
@@ -14,9 +15,12 @@ client.on('ready', (c) =>{
 });
 
 client.on('messageCreate', (message)=>{
+    if(message.author.bot){
+        return;
+    }
     if(message.content === 'Hey Kettle'){
         message.reply('Hey!');
     }
 });
 
-client.login('MTMxNDc4OTMyNzIxNzEwMjg5OQ.GELu60.VXOWRtewBENpjIS3_BjrpV62-03-Fbcfp_NqL8');
+client.login(process.env.TOKEN);
