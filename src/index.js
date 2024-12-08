90b4e98f55c9656e140ef73550f43fa733dddf01
Link <alexrexgamers@gmail.com>
2024-12-08 20:40:15 +0530

    bot can send embeds

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {Client, IntentsBitField} = require('discord.js');
+const {Client, IntentsBitField, EmbedBuilder, Embed} = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -37,6 +37,37 @@ client.on('interactionCreate', (interaction) =>{
         const num1 = interaction.options.get('first-number').value;
         const num2 = interaction.options.get('second-number').value;
         interaction.reply(`product: ${num1 * num2}`);
+    }
+    if(interaction.commandName === 'embed'){
+        const embed = new EmbedBuilder()
+        .setTitle('This is the embed title')
+        .setDescription('This is the description')
+        .setColor(0x121212)
+        .setAuthor({name:'Link', iconURL:'https://lh3.googleusercontent.com/5G-1E-QhiwLFrlJvl68PtEjOzfIUrP8kalXOP8YrNoLHE4T4pP4ovzi2iOjmu_z5_-TqklO0Dmy1qNXkuWNrGvTO=s1280-w1280-h800', url: 'https://youtu.be/u5_a-lQlv6A?list=PLpmb-7WxPhe0ZVpH9pxT5MtC4heqej8Es'})
+        .setThumbnail('https://lh3.googleusercontent.com/5G-1E-QhiwLFrlJvl68PtEjOzfIUrP8kalXOP8YrNoLHE4T4pP4ovzi2iOjmu_z5_-TqklO0Dmy1qNXkuWNrGvTO=s1280-w1280-h800')
+        .addFields({
+            name: 'Link\'s Field 1', 
+            value: 'Hello there', 
+            inline: true
+        },
+        {
+            name: 'Link\'s Field 2',
+            value: 'Good day we are having',
+            inline: true
+        },
+        {
+            name: '3rd field',
+            value: '234',
+
+        },
+        {
+            name: '4th field', 
+            value: '5435', 
+        }
+    
+        );
+
+        interaction.reply({embeds: [embed]});
     }
 });
 
